91ee39ad779d0adf8cf6d2f00c921736e21d075a
AmalaGB <142780308+AmalaGB@users.noreply.github.com>
2025-12-22 23:57:19 +0530

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -3,24 +3,25 @@ import Hero from "./components/Hero.tsx";
 import About from "./components/About.tsx";
 import Skills from "./components/Skills.tsx";
 import Projects from "./components/Projects.tsx";
-import Publications from "./components/Publications.tsx";
-import Blog from "./components/Blog.tsx";
 import Experience from "./components/Experience.tsx";
 import Contact from "./components/Contact.tsx";
 import Footer from "./components/Footer.tsx";
 
 function App() {
   return (
-    <div className="bg-slate-900">
+    <div className="bg-neutral-950 text-white overflow-x-hidden">
+
       <Navbar />
-      <Hero />
-      <Experience />
-      <Projects />
-      <Publications />
-      <Blog />
-      <Skills />
-      <About />
-      <Contact />
+
+      <main className="space-y-28">
+        <Hero />
+        <Skills />
+        <Experience />
+        <Projects />
+        <About />
+        <Contact />
+      </main>
+
       <Footer />
     </div>
   );
